7edfb40b068fcd9f4c2a17daad2e07cf7d67389b
孟伟 <77492982@qq.com>
2019-07-10 11:08:31 +0800

    39. Combination Sum

--- src/main/java/CountandSay.java
+++ src/main/java/CountandSay.java
@@ -1,7 +1,31 @@
-package PACKAGE_NAME;
-
 /**
+ * 38 Count and Say
  * Created by mengwei on 2019/7/9.
  */
 public class CountandSay {
+
+    public String countAndSay(int n) {
+        if(n == 1){
+            return "1";
+        }
+
+        String str = countAndSay(n - 1) + "*";
+        char[] c = str.toCharArray();
+        int count = 1;
+        String s = "";
+        for(int i = 0; i < c.length - 1;i++) {
+            if (c[i] == c[i + 1]) {
+                count++;//计数增加
+            } else {
+                s = s + count + c[i];//上面的*标记这里方便统一处理
+                count = 1;//初始化
+            }
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        String s = new CountandSay().countAndSay(5);
+        System.out.println("args = [" + s + "]");
+    }
 }
