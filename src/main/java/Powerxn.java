ff3a5f1bf5d3add1acf3c7310add522a16c80d16
孟伟 <77492982@qq.com>
2019-08-28 09:16:35 +0800

    powerxn

---
+++ src/main/java/Powerxn.java
@@ -0,0 +1,12 @@
+/**
+ * 直接模拟该过程，将 x 连乘 n 次。
+ * 如果 n < 0，我们可以用1/x
+ * −n 代替 x, nx,n 来保证 n \ge 0n≥0 。这个限制可以简化我们下面的讨论。
+ *
+ * Created by mengwei on 2019/8/28.
+ */
+public class Powerxn {
+    public double myPow(double x, int n) {
+
+    }
+}
